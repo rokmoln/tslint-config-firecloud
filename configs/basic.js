6637c0aeb15a64fba1c9674a57719a9e7a0aa465
Andrei Neculau <andrei.neculau@gmail.com>
2019-04-12 15:07:21 +0200

    mark compat with @typescript-eslint/eslint-plugin

--- configs/basic.js
+++ configs/basic.js
@@ -3,54 +3,68 @@
 
 module.exports = {
   rules: {
-    'adjacent-overload-signatures': true,
-    'array-type': [
-      true,
-      'array'
-    ],
-    'await-promise': true,
-    'ban-ts-ignore': false,
-    'ban-types': false,
-    'callable-types': true,
-    'interface-name': true,
-    'interface-over-type-literal': true,
+    // rules not (yet) supported by https://github.com/typescript-eslint/typescript-eslint
     'match-default-export-name': false,
-    'member-access': false,
-    'no-angle-bracket-type-assertion': true,
-    'no-any': false,
     'no-boolean-literal-compare': true,
-    'no-empty-interface': true,
     'no-floating-promises': true,
-    'no-inferrable-types': false,
     'no-inferred-empty-object-type': true,
-    'no-internal-module': true,
     'no-mergeable-namespace': true,
-    'no-misused-new': true,
-    'no-namespace': true,
-    'no-non-null-assertion': true,
     'no-null-undefined-union': true,
-    'no-object-literal-type-assertion': true,
-    'no-parameter-properties': true,
     'no-redundant-jsdoc': true,
     'no-reference-import': true,
-    'no-unbound-method': true,
-    'no-unnecessary-qualifier': true,
     'no-unsafe-any': true,
-    // ignored, not really typescriptOnly
-    // 'no-unused-variables': true,
-    // ignored, not really typescriptOnly
-    // 'no-var-requires': true,
-    // ignored, not really requiresTypeInfo
-    // 'no-use-before-declare': true, // requiresTypeInfo
     'prefer-readonly': false,
-    'promise-function-async': true, // requiresTypeInfo
-    'restrict-plus-operands': true, // requiresTypeInfo
     'return-undefined': false, // requiresTypeInfo
     'strict-boolean-expressions': true,
     'strict-type-predicates': true,
     'type-literal-delimiter': true,
     typedef: true,
-    'typedef-whitespace': [
+
+    // -------------------------------------------------------------------------
+
+    // rules supported by https://github.com/typescript-eslint/typescript-eslint
+    'adjacent-overload-signatures': true, // @typescript-eslint/adjacent-overload-signatures
+    'array-type': [ // @typescript-eslint/array-type
+      true,
+      'array'
+    ],
+    'await-promise': true, // @typescript-eslint/await-thenable
+    'ban-ts-ignore': false, // @typescript-eslint/ban-ts-ignore
+    'ban-types': false, // @typescript-eslint/ban-types
+    'callable-types': true, // @typescript-eslint/prefer-function-type
+    'interface-name': true, // @typescript-eslint/interface-name-prefix
+    'interface-over-type-literal': true, // @typescript-eslint/no-type-alias, @typescript-eslint/prefer-interface
+    'member-access': false, // @typescript-eslint/explicit-member-accessibility
+    'member-ordering': [ // @typescript-eslint/member-ordering
+      true,
+      {
+        order: 'fields-first'
+      }
+    ],
+    'no-angle-bracket-type-assertion': true, // @typescript-eslint/no-angle-bracket-type-assertion
+    'no-any': false, // @typescript-eslint/no-explicit-any
+    'no-empty-interface': true, // @typescript-eslint/no-empty-interface
+    'no-inferrable-types': false, // @typescript-eslint/no-inferrable-types
+    'no-internal-module': true, // @typescript-eslint/prefer-namespace-keyword
+    'no-misused-new': true, // @typescript-eslint/no-misused-new
+    'no-namespace': true, // @typescript-eslint/no-namespace
+    'no-non-null-assertion': true, // @typescript-eslint/no-non-null-assertion
+    'no-object-literal-type-assertion': true, // @typescript-eslint/no-object-literal-type-assertion
+    'no-parameter-properties': true, // @typescript-eslint/no-parameter-properties
+    // typescript specific but not marked typescriptOnly
+    'no-reference': true, // @typescript-eslint/no-triple-slash-reference
+    'no-unbound-method': true, // @typescript-eslint/unbound-method
+    'no-unnecessary-qualifier': true, // @typescript-eslint/no-unnecessary-qualifier
+    'no-unnecessary-type-assertion': true, // @typescript-eslint/no-unnecessary-type-assertion
+    // ignored, not really typescriptOnly
+    // 'no-unused-variables': true, // // @typescript-eslint/no-unused-vars
+    // ignored, not really typescriptOnly
+    // 'no-var-requires': true, // @typescript-eslint/no-var-requires
+    // ignored, not really requiresTypeInfo
+    // 'no-use-before-declare': true, // requiresTypeInfo, @typescript-eslint/no-use-before-define
+    'promise-function-async': true, // requiresTypeInfo, @typescript-eslint/promise-function-async
+    'restrict-plus-operands': true, // requiresTypeInfo, @typescript-eslint/restrict-plus-operands
+    'typedef-whitespace': [ // @typescript-eslint/type-annotation-spacing
       true,
       {
         'call-signature': 'nospace',
@@ -67,6 +81,28 @@ module.exports = {
         'variable-declaration': 'onespace'
       }
     ],
-    'unified-signatures': true
+    'unified-signatures': true // @typescript-eslint/unified-signatures
+
+    // -------------------------------------------------------------------------
+
+    // rules in https://github.com/typescript-eslint/typescript-eslint, typescript specific
+    // @typescript-eslint/member-delimiter-style
+    // @typescript-eslint/member-naming
+
+    // -------------------------------------------------------------------------
+
+    // rules in https://github.com/typescript-eslint/typescript-eslint, but not typescript specific
+    // @typescript-eslint/camelcase
+    // 'class-name': undefined, @typescript-eslint/class-name-casing
+    // @typescript-eslint/explicit-function-return-type
+    // @typescript-eslint/generic-type-naming
+    // 'indent', @typescript-eslint/indent
+    // @typescript-eslint/no-array-constructor
+    // 'no-unnecessary-class': undefined, // @typescript-eslint/no-extraneous-class
+    // 'no-require-imports': undefined, // @typescript-eslint/no-require-imports
+    // 'no-this-assignment': undefined, // @typescript-eslint/no-this-alias
+    // 'no-unused-variable': undefined,
+    // @typescript-eslint/no-useless-constructor
+    // @typescript-eslint/prefer-for-of
   }
 };
